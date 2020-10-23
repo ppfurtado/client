5d311d5a362fb712fc7db63bcc85b32e80f19d52
Pedro Furtado <furtado.pedropaulo@gmail.com>
2020-10-22 22:09:57 -0300

    add config GlobalStyle

---
+++ src/pages/__app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps /*, AppContext */ } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyle from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplates</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starte t work with Typescript,NextJs and Styled Component"
+        />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
