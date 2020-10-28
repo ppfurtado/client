0adb9713d5b8b70f9dd0439126d5eb96d45fc9ee
Pedro Furtado <furtado.pedropaulo@gmail.com>
2020-10-27 23:18:29 -0300

    add snapshot and storybook

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,10 +4,20 @@ import Main from '.'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    render(<Main />)
+    const { container } = render(<Main />)
 
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render color correct', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b'
+    })
   })
 })
