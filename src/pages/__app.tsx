6c569e49d3f202c28f0e70bcf40ac732b18a156a
Pedro Furtado <furtado.pedropaulo@gmail.com>
2020-11-04 16:18:44 -0300

    swap project-name

--- src/pages/__app.tsx
+++ src/pages/__app.tsx
@@ -9,6 +9,7 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>React Avançado - Boilerplates</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta
           name="description"
