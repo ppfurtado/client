92855815e96d7e841c4a217efbaa42387fcd3daa
Pedro Furtado <furtado.pedropaulo@gmail.com>
2020-11-09 22:55:47 -0300

    fix conflict

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   collectCoverage: true,
-<<<<<<< HEAD
-=======
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
->>>>>>> 8aea7cee29b9d90b6a5ac1c88c2758bc557c37d6
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   moduleNameMapper: {
